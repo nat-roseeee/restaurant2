97f89eac9d127f7070a6a3478508e223acc173cc
Natalie Baiocchi <natalie.baiocchi@infinitecampus.com>
2022-08-01 10:24:43 -0500

    updated generator component

--- restaurant-generator-2/src/generator/generator.component.ts
+++ restaurant-generator-2/src/generator/generator.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './generator.component.html',
+  styleUrls: ['./generator.component.css']
 })
 export class AppComponent {
   title = 'test-app';
